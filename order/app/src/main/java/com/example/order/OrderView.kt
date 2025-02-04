d7610449c13aeaab7798629a4cd80113f9b256ab
Tomisuke <searakko3@gmail.com>
2025-02-04 18:10:33 +0900

    topView完成。orderView作成中

---
+++ order/app/src/main/java/com/example/order/OrderView.kt
@@ -0,0 +1,42 @@
+package com.example.order
+
+import android.provider.MediaStore.Audio.Radio
+import android.widget.RadioButton
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.selection.selectable
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.RadioButton
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun RadioButtonWithText(
+    text: String,
+    selected: Boolean,
+    onSelect: () -> Unit
+) {
+    Row(
+        modifier = Modifier
+            .padding(8.dp)
+            .fillMaxWidth()
+            .selectable(selected = selected, onClick = onSelect),
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        RadioButton(selected = selected, onClick = null)
+        Text(
+            text = text, style = MaterialTheme.typography.bodyLarge
+        )
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+private fun RadioButtonWithTextPreview() {
+    RadioButtonWithText(text = "ハンバーガー", selected = true, onSelect = {})
+}
