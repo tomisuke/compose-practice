df9438ad68a7b2a84f8848e600d84b56871f2977
Tomisuke <searakko3@gmail.com>
2025-02-11 21:08:07 +0900

    feat: アプリケーション全体の完成

--- order/app/src/main/java/com/example/order/OrderView.kt
+++ order/app/src/main/java/com/example/order/OrderView.kt
@@ -1,17 +1,33 @@
 package com.example.order
 
 
+import androidx.annotation.DrawableRes
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.selection.selectable
 import androidx.compose.foundation.selection.selectableGroup
 import androidx.compose.foundation.selection.toggleable
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowDropDown
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Checkbox
+import androidx.compose.material3.DropdownMenu
+import androidx.compose.material3.DropdownMenuItem
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.RadioButton
 import androidx.compose.material3.Slider
 import androidx.compose.material3.Text
@@ -22,10 +38,70 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.order.ui.theme.BaseColor
+import com.example.order.ui.theme.Orange400
+import com.example.order.ui.theme.Pink80
+
+@Composable
+fun OrderView(
+    @DrawableRes imageRes: Int = R.drawable.classicbeef,
+    onTapButton: () -> Unit = {}
+) {
+    val scrollState = rememberScrollState()
+    Column(
+        modifier = Modifier
+            .padding(16.dp)
+            .verticalScroll(scrollState),
+        verticalArrangement = Arrangement.spacedBy(16.dp),
+        horizontalAlignment = Alignment.CenterHorizontally,
+    ) {
+        Image(
+            painter = painterResource(id = R.drawable.order_screen),
+            contentDescription = "order screen",
+            modifier = Modifier.fillMaxWidth(),
+            contentScale = ContentScale.FillWidth,
+        )
+        Box(
+            modifier = Modifier
+                .padding(32.dp)
+                .fillMaxWidth(),
+            contentAlignment = Alignment.Center
+        ) {
+            Image(
+                painter = painterResource(
+                    id = R.drawable.order_background
+                ),
+                contentDescription = "hamburger",
+            )
+            Image(
+                painter = painterResource(id = imageRes),
+                contentDescription = "hamburger",
+                modifier = Modifier.fillMaxSize(),
+                contentScale = ContentScale.Crop
+            )
+        }
+        MainDishSection()
+        SideMenuSection()
+        SauceAmountSection()
+        DrinkSelectionSection()
+        OrderButtonSection("注文する", onTapButton)
+    }
+}
+
+@Preview(showBackground = true, widthDp = 400, heightDp = 1200, backgroundColor = 0xFF000000)
+@Composable
+private fun OrderViewPreview() {
+    OrderView()
+}
 
 @Composable
 fun RadioButtonWithText(
@@ -101,8 +177,7 @@ fun SideMenuSection() {
         ) {
             Text("サイドメニュー", style = MaterialTheme.typography.titleLarge)
             Row(
-                modifier = Modifier.toggleable(
-                    value = frenchFries,
+                modifier = Modifier.toggleable(value = frenchFries,
                     onValueChange = { frenchFries = it }),
                 verticalAlignment = Alignment.CenterVertically
             ) {
@@ -153,4 +228,88 @@ fun SauceAmountSection() {
 @Composable
 private fun SauceAmountSectionPreview() {
     SauceAmountSection()
+}
+
+@Composable
+fun DrinkSelectionSection() {
+    var expanded by remember { mutableStateOf(false) }
+    var selectedDrink by remember { mutableStateOf("選択してください") }
+    val drinks = listOf("アイスコーヒー", "アイスカフェオレ", "コーラ")
+    Box(
+        modifier = Modifier
+            .fillMaxWidth()
+            .background(BaseColor, shape = MaterialTheme.shapes.extraLarge)
+    ) {
+        Column(
+            modifier = Modifier.padding(16.dp)
+        ) {
+            Text("ドリンクを選択してください", style = MaterialTheme.typography.titleLarge)
+            Box(
+                modifier = Modifier
+                    .padding(16.dp)
+                    .fillMaxWidth()
+            ) {
+                OutlinedTextField(
+                    value = selectedDrink,
+                    onValueChange = {},
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .clickable { expanded = true },
+                    trailingIcon = {
+                        Icon(
+                            Icons.Filled.ArrowDropDown,
+                            "dropdown",
+                            Modifier.clickable { expanded = true },
+                        )
+                    },
+                    label = { Text("ドリンク") },
+                    readOnly = true
+                )
+                DropdownMenu(expanded = expanded, onDismissRequest = { expanded = false }) {
+                    drinks.forEach { drink ->
+                        DropdownMenuItem(text = { Text(text = drink) }, onClick = {
+                            selectedDrink = drink
+                            expanded = false
+                        })
+                    }
+                }
+            }
+        }
+    }
+}
+
+@Preview(showBackground = true, backgroundColor = 0xFF000000)
+@Composable
+private fun DrinkSelectionSectionPreview() {
+    DrinkSelectionSection()
+}
+
+@Composable
+fun OrderButtonSection(text: String, onClick: () -> Unit = {}) {
+    val gradient = Brush.verticalGradient(
+        colors = listOf(Pink80, Orange400)
+    )
+    Button(
+        onClick = onClick,
+        colors = ButtonDefaults.buttonColors(
+            containerColor = Color.Transparent
+        ),
+        contentPadding = PaddingValues(),
+        modifier = Modifier
+            .fillMaxWidth()
+            .background(gradient, shape = MaterialTheme.shapes.extraLarge)
+    ) {
+        Text(
+            text = text,
+            color = Color.White,
+            fontWeight = FontWeight.Bold,
+            fontSize = 40.sp
+        )
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+private fun OrderButtonSectionPreview() {
+    OrderButtonSection("注文する")
 }
