8d7e3a716199e2a2b01e9790119868f6810c63f8
Tomisuke <searakko3@gmail.com>
2025-02-14 19:59:41 +0900

    feat: Scaffoldでベースを作成

--- CountDownTimer/app/src/main/java/com/example/countdowntimer/MainActivity.kt
+++ CountDownTimer/app/src/main/java/com/example/countdowntimer/MainActivity.kt
@@ -4,14 +4,23 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Timer
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.Icon
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.SmallFloatingActionButton
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.countdowntimer.ui.theme.CountDownTimerTheme
+import com.example.countdowntimer.ui.theme.Pink80
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -19,29 +28,43 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             CountDownTimerTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
             }
         }
     }
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
+fun ExampleScaffold() {
+    Scaffold(
+        topBar = { Text("TopAppBar") },
+        bottomBar = { Text("BottomAppBar") },
+        floatingActionButton = {
+            FloatingActionButton(
+                onClick = {/*TODO*/ },
+                content = {
+                    Icon(
+                        imageVector = Icons.Filled.Timer,
+                        contentDescription = "Timer",
+                    )
+                }
+            )
+        },
+        content = { innerPadding ->
+            Box(
+                modifier = Modifier
+                    .padding(innerPadding)
+                    .fillMaxSize()
+                    .background(color = Pink80),
+                contentAlignment = Alignment.Center
+            ) {
+                Text(text = "content")
+            }
+        }
     )
 }
 
-@Preview(showBackground = true)
+@Preview(widthDp = 300, heightDp = 500)
 @Composable
-fun GreetingPreview() {
-    CountDownTimerTheme {
-        Greeting("Android")
-    }
+private fun ExampleScaffoldPreview() {
+    ExampleScaffold()
 }
