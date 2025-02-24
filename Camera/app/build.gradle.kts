4c96b52b10719c5a0d51623159df95376347a7d9
Tomisuke <searakko3@gmail.com>
2025-02-25 00:12:40 +0900

    feat/カメラの状態管理クラスを作成

--- Camera/app/build.gradle.kts
+++ Camera/app/build.gradle.kts
@@ -55,6 +55,7 @@ dependencies {
     implementation(libs.androidx.camera.view)
     implementation(libs.androidx.camera.extensions)
     implementation(libs.camera.core)
+    implementation(libs.androidx.viewfinder.core)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
--- Camera/app/src/main/java/com/example/mycamera/CameraState.kt
+++ Camera/app/src/main/java/com/example/mycamera/CameraState.kt
@@ -1,21 +1,46 @@
 package com.example.mycamera
 
 import android.content.Context
+import android.util.Log
 import androidx.camera.core.ImageCapture
+import androidx.camera.core.Preview
 import androidx.camera.lifecycle.ProcessCameraProvider
 import androidx.camera.view.PreviewView
+import androidx.core.content.ContextCompat
 import androidx.lifecycle.LifecycleOwner
 import com.google.common.util.concurrent.ListenableFuture
 
-data class CameraState (
+data class CameraState(
     val context: Context,
     val cameraProviderFuture: ListenableFuture<ProcessCameraProvider>,
     val lifecycleOwner: LifecycleOwner,
     val imageCapture: ImageCapture,
-){
-    fun startCamera(ctx: Context): PreviewView{
-        val previewView = PreviewView(ctx).apply{
-            implementationMode = Preweiw
+) {
+    fun startCamera(ctx: Context): PreviewView {
+        val previewView = PreviewView(ctx).apply {
+            implementationMode = PreviewView.ImplementationMode.COMPATIBLE
         }
+        val executor = ContextCompat.getMainExecutor(ctx)
+        cameraProviderFuture.addListener(
+            {
+                val cameraProvider = cameraProviderFuture.get()
+                val preview = androidx.camera.core.Preview.Builder().build().also {
+                    it.setSurfaceProvider(previewView.surfaceProvider)
+                }
+                val cameraSelector = androidx.camera.core.CameraSelector.DEFAULT_BACK_CAMERA
+                try {
+                    cameraProvider.unbindAll()
+                    cameraProvider.bindToLifecycle(
+                        lifecycleOwner,
+                        cameraSelector,
+                        preview,
+                        imageCapture,
+                    )
+                } catch (e: Exception) {
+                    Log.e("cameraPreview", "Use case binding failed", e)
+                }
+            }, executor
+        )
+        return previewView
     }
 }
